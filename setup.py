6519c219de712c84b68621aff5a00656d5f561a5
Marie PONCHART <marie.ponchart@ifremer.fr>
2018-04-19 11:18:57 +0200

    reorganisation des fichiers utilitaires

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='clib',
+      description='cognac acoustic utility library',
+      url='https://github.com/ponchama/Cognac_Acoustique',
+      packages=['clib'])
+
